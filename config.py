c5febb668330987a314cfc597a2f42fe7d5d8ff6
kidus workneh abebe <kidusw@localhost.localdomain>
2026-04-09 18:19:32 +0300

    file uploaded functionality implementation

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+
 from pydantic import SecretStr
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
@@ -11,6 +12,8 @@ class Settings(BaseSettings):
     secret_key: SecretStr
     algorithm: str = "HS256"
     access_token_expire_minutes: int = 30
+
+    max_upload_size_bytes: int = 5 * 1024 * 1024
 
 
 settings = Settings()  # type: ignore[call-arg] # Loaded from .env file
